9715edb5c740628520385ac87bf5fcf8d54c9a44
boymeetsgithub <deboomjoe@gmail.com>
2023-06-09 14:41:27 -0500

    Anotehr bug fix.

--- teach_sam.py
+++ teach_sam.py
@@ -88,8 +88,8 @@ if __name__ == "__main__":
         print(img)
         print(img.shape)
         print(img.dtype)
-        #img = np.float64(img)
-        #print(img.dtype)
+        img = img.astype(np.uint8)
+        print(img.dtype)
 
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
